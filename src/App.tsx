b91edc204bf837a161788bef8609cd16d1c2c393
deinde dawodu <ddawodu@brightsign.biz>
2023-02-21 14:39:12 -0500

    adding in  examples to type functions also to  handle typing props and events

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import List from "./components/List"
+import List from "./components/List";
+import AddToList from "./components/AddToList";
 
 function App() {
   //HOW TO ADD TYPESCRIPT TO STATE!!
@@ -22,22 +23,22 @@ function App() {
   //BELOW IS IMPLEMENTING AND OVERIDING THE "INFERED" TYPE AND DEFININIG AN OVERIDING CUSTOM TYPE OF array of people
   //AND SETTING INITIAL STATE TO EMPTY ARRAY!
   //"https://fullpresscoverage.com/wp-content/uploads/2020/01/101524695-457220551.jpg"
-  
+
   const [people, setPeople] = useState<IState["people"]>([
     {
-      name:"Lebron James",
-      url:"https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png",
-      age:36,
-      note:"Allergic to staying on the same team"
-    }
+      name: "Lebron James",
+      url: "https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png",
+      age: 36,
+      note: "Allergic to staying on the same team",
+    },
   ]);
-  
-  
+
   people.map((peep) => console.log(peep.name));
   return (
     <div className="App">
       <h1>People Invited To Our Party</h1>
-      <List people={people}/>
+      <List people={people} />
+      <AddToList/>
     </div>
   );
 }
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -22,20 +22,48 @@ interface IProps {
 //   )
 // }
 
-//THIS IS THE BEST PRACTICE WAY TO BE MORE SPECIFIC TO DO THIS!!!
+//THIS IS THE BEST PRACTICE WAY TO BE MORE SPECIFIC TO DO DECLARE FUNCTION TYPE!!!
 
+// const List: React.FC<IProps> = ({ people }) => {
+//   return (
+//     //BELOW WORKS BUT THE BETTER WAY IS TO MAKE A FUNCTION SEPERATE THEN CALL IT!!
+//     <ul>
+//       {people.map((peep) => (
+//         <div>
+//           <img src={peep.url} />
+//           <h1>{peep.name}</h1>
+//           <p>{peep.age}</p>
+//           <p>{peep.note}</p>
+//         </div>
+//       ))}
+//     </ul>
+//   );
+// };
+
+//BELOW IS THE BEST WAY TO DECLARE AND RENDER A TYPSCRIPT REACT APP BEST PRACTICE
 const List: React.FC<IProps> = ({ people }) => {
+    
+    //HOW TO TELL TYPESCRIPT I AM RETURNING A JSX ELEMENT. INSTEAD OF LETTING IT INFER!!
+    const renderList = ():JSX.Element[]=>{
+        return people.map((peep)=>(
+          <li className="List" key={peep.name}>
+            <div className="List-header">
+              <img className="List-img" src={peep.url} alt="" />
+              <h2>{peep.name}</h2>
+              </div>
+              <p>{peep.age}</p>
+              <p className="List-note">{peep.note}</p>
+          </li>
+        ))
+           
+        
+    }
+   
   return (
     //BELOW WORKS BUT THE BETTER WAY IS TO MAKE A FUNCTION SEPERATE THEN CALL IT!!
     <ul>
-      {people.map((peep) => (
-        <div>
-          <img src={peep.url} />
-          <h1>{peep.name}</h1>
-          <p>{peep.age}</p>
-          <p>{peep.note}</p>
-        </div>
-      ))}
+            {renderList()}
+
     </ul>
   );
 };
