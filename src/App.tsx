ad208cc8b7f87a7e532a5094f338e7713ca24ace
deinde dawodu <ddawodu@brightsign.biz>
2023-02-21 11:45:21 -0500

    adding notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  //HOW TO ADD TYPESCRIPT TO STATE!!
+  //BELOW IS TYPE DEFINTION
+
+  interface IState {
+    people: {
+      name: string;
+      url: string;
+      age: number;
+      note?: string;
+    }[];
+  }
+
+  //Above Notice you must read from end to top NOT TOP TO END!! READ ARRAY OF OBJECTS CALLED PEOPLE!!!
+
+  //HOW TO ADD TYPESCRIPT TO STATE!!
+  //BELOW IS IMPLEMENTING AND OVERIDING THE "INFERED" TYPE AND DEFININIG AN OVERIDING CUSTOM TYPE OF array of people
+  const [people, setPeople] = useState<IState["people"]>([]);
+  people.map((peep) => console.log(peep.name));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People Invited To Our Party</h1>
     </div>
   );
 }
